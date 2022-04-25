52f60f1ea62095535991ba2ed1f83e968c6863e6
Mohammed <gsft22221@gmail.com>
2022-04-25 08:54:48 +0200

    Update randomString.ts

--- src/randomString.ts
+++ src/randomString.ts
@@ -1,5 +1,9 @@
+import chalk from 'chalk'
+
 export default {
-    randomString(length: number, characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz*-._"): string {
+    randomString(length: number = 8, characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz*-._"): string {
+        if (!length) console.warn(`${chalk.yellow("»")} randomString() requires the following argument: length (default is 8)`);
+
         var result = "";
 
         for (var c = 0; c < length; c++) {
