7dd70652b1b13fecdaddd8debcaa0a342d859f62
Mohammed <gsft22221@gmail.com>
2022-04-25 08:55:02 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import randomBoolean from "./randomBoolean"
-// import randomColor from "./randomColor"
+import randomColor from "./randomColor"
 import randomEmoji from "./randomEmoji"
 import randomInt from "./randomInt"
 import randomString from "./randomString"
+import randomAnimal from "./randomAnimal"
+import randomCountry from "./randomCountry"
+
+import array from "./array"
 
 export default {
     ...randomEmoji,
     ...randomInt,
     ...randomBoolean,
     ...randomString,
-    ...randomColor
+    ...randomColor,
+    ...randomAnimal,
+    ...randomCountry,
+    ...array
 }
